df679ece581f7eb518e4741f791c3b7e77954ae7
Newball <newball875@gmail.com>
2026-07-26 14:28:02 +0200

    Rajout infos dans __init__

---
+++ src/newball-sql/__init__.py
@@ -0,0 +1,14 @@
+from .ws import creerSession, finJob, WS
+from .utils import readYAML, slugify, findImage, copieImage, createDossier
+
+__all__ = [
+	creerSession,
+	finJob,
+	readYAML,
+	slugify,
+	findImage,
+	copieImage,
+	createDossier,
+
+	WS
+]
