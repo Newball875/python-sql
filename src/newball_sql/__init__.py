58bc453f6e80a33ada8a9eafb34653362395ac4a
Newball <newball875@gmail.com>
2026-08-28 11:15:27 +0200

    Rajout méthodes

--- src/newball_sql/__init__.py
+++ src/newball_sql/__init__.py
@@ -1,9 +1,10 @@
-from .ws import creerSession, finJob, WS, Base, Migrations
+from .ws import creerSession, finJob, runMigrations, WS, Base, Migrations
 from .utils import readYAML, slugify, findImage, copieImage, createDossier
 
 __all__ = [
 	creerSession,
 	finJob,
+	runMigrations,
 	readYAML,
 	slugify,
 	findImage,
